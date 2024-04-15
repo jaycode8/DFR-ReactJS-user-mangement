e6c8e8812d909e15acb55101a82d810ba35950cb
jaycode8 <jamesmumo443@gmail.com>
2024-04-15 23:08:57 +0300

    configured CORS

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:5173",
+    "*",
 ]
 
 AUTHENTICATION_BACKENDS = [
